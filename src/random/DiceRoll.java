c0197d68110f2161ae5bfdacf319469ddd65dc49
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-09 10:54:19 -0800

    Dice Roll validation rock paper scissors

--- src/random/DiceRoll.java
+++ src/random/DiceRoll.java
@@ -51,7 +51,7 @@ public class DiceRoll {
 
 			// 1. Make randomChoice equal to a random number between 1 and 6
 			
-int randomChoice=new Random().nextInt(6)+1;
+int randomChoice=new Random().nextInt(7)+1;
 			// 2. Fix the code below so that it displays the correct image
 		     if (randomChoice == 1) {
 				label.setIcon(oneImg);
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -19,8 +19,13 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
+if(randomNumber==0){JOptionPane.showMessageDialog(null,"Your nice");}
+else if(num==1){JOptionPane.showMessageDialog(null,"your looks nice");}
+else if(num==2) {JOptionPane.showMessageDialog(null, "");}
+else if(num==3){JOptionPane.showMessageDialog(null, message);}
+else(num==4){JOptionPane.showMessageDialog(null, message);}
 
-		// 4. Repeat all the code above 10 times
+// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
 	}
